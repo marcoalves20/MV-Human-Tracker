93b8264f7a3b230b5fa1ac75e7ec25b3f97c73f9
marcoalves <marco@move.ai>
2022-06-17 08:39:18 +0100

    added detector data

---
+++ detector.py
@@ -0,0 +1,49 @@
+import yolov5
+import cv2
+import torch
+
+def filter_predictions(results):
+  predictions = results.pred[0]
+  categories = predictions[:, 5]
+  human_idx = (categories == 0).nonzero()
+  predictions = results.pred[0][human_idx]
+  predictions = predictions[human_idx]
+  boxes = predictions[:, :4]
+  boxes = boxes[human_idx]
+  return torch.squeeze(predictions), torch.squeeze(boxes)
+
+def draw_predictions(frame, boxes):
+  boxes = boxes.numpy().astype(int)
+
+  for i in range(boxes.shape[0]):
+    cv2.rectangle(frame, (boxes[i,0], boxes[i,1]), (boxes[i,2], boxes[i,3]), (0, 255, 0), 2)
+  return frame
+
+
+model = yolov5.load('yolov5s.pt')
+model.conf = 0.25  # NMS confidence threshold
+model.iou = 0.45  # NMS IoU threshold
+model.agnostic = False  # NMS class-agnostic
+model.multi_label = False  # NMS multiple labels per box
+model.max_det = 30  # maximum number of detections per image
+
+cap = cv2.VideoCapture('videos/cam01.mp4')
+while(cap.isOpened()):
+  ret, frame = cap.read()
+  # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+  results = model(frame)
+  prediction, boxes = filter_predictions(results)
+  frame = draw_predictions(frame, boxes)
+  cv2.imshow('frame', frame)
+
+  if cv2.waitKey(1) & 0xFF == ord('q'):
+    break
+
+
+
+# predictions = results.pred[0]
+# boxes = predictions[:, :4]  # x1, y1, x2, y2
+# scores = predictions[:, 4]
+# categories = predictions[:, 5]
+
+# show detection bounding boxes on image
