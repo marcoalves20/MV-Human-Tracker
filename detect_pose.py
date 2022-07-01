07d00c4649edd8d288da215eb95aa27d7434e6a0
vagia <vagia@move.ai>
2022-07-01 12:44:20 +0300

    Add pose cost in match_multiview/ refactoring

--- detect_pose.py
+++ detect_pose.py
@@ -2,11 +2,10 @@ import cv2
 from detector_module import HumanDetector
 from pose_module import PoseDetector
 import time
-import mediapipe as mp
-
-import matplotlib
-matplotlib.use('TkAgg')
+import numpy as np
 import matplotlib.pyplot as plt
+import pickle
+
 
 def debug_plot(croped_img, poses):
     plt.imshow(croped_img)
@@ -14,35 +13,63 @@ def debug_plot(croped_img, poses):
     plt.show()
 
 
-mpPose = mp.solutions.pose
-pose = mpPose.Pose()
-mpDraw = mp.solutions.drawing_utils
-cap = cv2.VideoCapture('videos/wembley/cam01.mp4')
+cap = cv2.VideoCapture('videos/wembley/cam02.mp4')
 pTime = 0
+kps_thres = 0.7
 
-yolo_det = HumanDetector()
-pose_det = PoseDetector()
-
+yolo_det = HumanDetector(img_size=1920)
+pose_det = PoseDetector(static_image_mode=True, model_complexity=1, enable_segmentation=False,
+                        min_detection_confidence=0.4)
+detections = []
 while True:
     success, img = cap.read()
     # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     bboxes = yolo_det.predict(img)
+    scores = bboxes[:, 4]
 
     poses = []
     for i in range(bboxes.shape[0]):
-        bboxes[i,:], cropped_img = yolo_det.get_bb_img(img, bboxes[i,:], expand_bb=True, margin=5)
+        # if bbox score is smaller than a threshold ignore it
+        if scores[i] < 0.5:
+            break
+
+        detections.append(bboxes[i, :])
+        cropped_img = yolo_det.get_bb_img(img, bboxes[i, :], expand_bb=True, margin=5)
         poses.append(pose_det.predict(cropped_img))
-        h, w, c = cropped_img.shape
-        for j in range(poses[i].shape[0]):
-            cx, cy = int(poses[i][j,0] ), int(poses[i][j,1] )
-            cropped_img=cv2.circle(cropped_img, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
 
-        cTime = time.time()
-        fps = 1 / (cTime - pTime)
-        pTime = cTime
+        if np.all(poses[i] != 0):
+            for j in range(poses[i].shape[0]):
+                cx, cy = int(poses[i][j,0] ), int(poses[i][j,1] )
+                x, y = int(poses[i][j,0] + bboxes[i][0]), int(poses[i][j,1] + bboxes[i][1])
+                if poses[i][j, 2] > kps_thres:
+                    cv2.circle(cropped_img, (cx, cy), 3, (255, 0, 0), cv2.FILLED)
+                    cv2.circle(img, (x, y), 3, (255, 0, 0), cv2.FILLED)
+                else:
+                    cv2.circle(cropped_img, (cx, cy), 3, (0, 0, 255), cv2.FILLED)
+                    cv2.circle(img, (x, y), 3, (0, 0, 255), cv2.FILLED)
 
-        cv2.imshow("Image", cropped_img)
+        cv2.imshow("Cropped Image", cropped_img)
         cv2.waitKey(1)
-        print(i)
+
+    # Create detections - poses dictionary and save it.
+    detections_dict = {'detections': np.array(detections), 'poses': np.array(poses)}
+    # with open("detections/detections_dict_cam02.pkl", "wb") as f:
+    #     pickle.dump(detections_dict, f)
+    #     f.close()
+
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+
+    # resize image
+    scale_percent = 50  # percent of original size
+    width = int(img.shape[1] * scale_percent / 100)
+    height = int(img.shape[0] * scale_percent / 100)
+    dim = (width, height)
+
+    resized = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
+    cv2.putText(resized, str(int(fps)), (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 3)
+    cv2.imshow("Image", resized)
+    cv2.waitKey(1)
 
 
--- match_multiview.py
+++ match_multiview.py
@@ -8,6 +8,9 @@ import time
 from utils.camera_utils import load_calibration
 from utils.twoview_geometry import fundamental_from_poses, compute_epilines, distance_point_line
 from scipy.optimize import linear_sum_assignment
+import pickle
+import matplotlib.pyplot as plt
+
 
 class Detection(object):
 
@@ -38,9 +41,78 @@ class Detection2D(Detection):
             self=self)
 
 
+def calc_cost_poses(poses_1, poses_2, F, kps_thres=0.8):
 
-def find_candidate_matches(detections, views, calibration, max_dist=10, n_candidates=2,
-                           verbose=0):
+    cost_poses = np.zeros((len(poses_1), len(poses_2)))
+
+    for i, pose1 in enumerate(poses_1):
+        if np.all(pose1 == 0):
+            cost_poses[i, :] = np.nan
+            continue
+
+        _, lines = compute_epilines(pose1[:, :2], None, F)  # 33 lines (equal to the number of keypoints)
+
+        # calculate distance from epipolar lines for each pose in second view
+        for id, pose2 in enumerate(poses_2):
+            distances = []
+            for pt_id in range(pose2.shape[0]):
+                if pose2[pt_id, 2] > kps_thres and pose1[pt_id, 2] > kps_thres:
+                    distances.append(distance_point_line(pose2[pt_id, :2], lines[pt_id, :]))
+
+            if len(distances) != 0:
+                cost_poses[i, id] = np.nanmedian(distances)
+            else:
+                cost_poses[i, id] = np.nan
+
+    return cost_poses
+
+
+def calc_cost_detections(dets1, dets2, F, view1, view2, max_dist=10, n_candidates=2, verbose=0):
+    """ It calculates the cost matrix based on bbox detections and returns candidate matches based on this."""
+
+    cost = np.zeros((len(dets1), len(dets2)))
+    sel_ids = []
+
+    _, lines = compute_epilines(dets1, None, F)
+
+    for i1, line in enumerate(lines):
+
+        distances = [distance_point_line(x, line) for x in dets2]
+        cost[i1, :] = distances
+        idx_sorted = np.argsort(distances)
+        idxs_candidates = []
+        sel_distances = []
+        for idx in idx_sorted:
+            # exit this loop if the distance start to be
+            # o high or the number candidates is reached
+            if verbose == 2:
+                print("{}-{} {}-{} {:0.2f}".format(view1, i1, view2, idx, distances[idx]))
+            if distances[idx] > max_dist:
+                if verbose == 2:
+                    print("{}-{} {}-{} discarded because of distance {:0.2f}".format(view1, i1, view2, idx,
+                                                                                     distances[idx]))
+                else:
+                    break
+            elif len(idxs_candidates) >= n_candidates:
+                if verbose == 2:
+                    print("{}-{} {}-{} discarded because of number of candidates reached.".format(view1, i1,
+                                                                                                  view2,
+                                                                                                  idx))
+                else:
+                    break
+            else:
+                if verbose == 2:
+                    print("{}-{} {}-{} selected distance {:0.2f}".format(view1, i1, view2, idx,
+                                                                         distances[idx]))
+                idxs_candidates.append(idx)
+                sel_distances.append(distances[idx])
+
+        sel_ids.append((idxs_candidates, sel_distances))
+
+    return cost, sel_ids
+
+
+def find_candidate_matches(detections, poses, views, calibration, max_dist=10, n_candidates=2, verbose=0):
     """
     Given a detection in one view, find the best candidates detections on the other views
 
@@ -58,9 +130,9 @@ def find_candidate_matches(detections, views, calibration, max_dist=10, n_candid
         max number of candidates per detection in each view
     """
 
-    for view, ds in detections.items():
-        K = np.array(calibration[view]['K'])
-        dist = np.array(calibration[view]['dist'])
+    # for view, ds in detections.items():
+    #     K = np.array(calibration[view]['K'])
+    #     dist = np.array(calibration[view]['dist'])
         # for d in ds:
         #     d.position_undist = cv2.undistortPoints(np.reshape(d.position, (1, 2)), K, dist, P=K)[0].squeeze()
 
@@ -81,6 +153,8 @@ def find_candidate_matches(detections, views, calibration, max_dist=10, n_candid
         positions_undist1 = np.reshape([detection.position
                                         for detection in detections[view1]], (-1, 2))
 
+        poses_1 = poses[view1]
+
         for view2 in views:
 
             if view1 != view2:
@@ -97,48 +171,21 @@ def find_candidate_matches(detections, views, calibration, max_dist=10, n_candid
                     sel_indexes[view1][view2] = [([], [])] * len(detections[view1])
                     continue
 
+                # Calculate detection cost
                 positions_undist2 = np.reshape([detection.position
                                                 for detection in detections[view2]], (-1, 2))
-
-                _, lines2 = compute_epilines(positions_undist1, None, F)
-                cost = np.zeros((len(positions_undist1), len(positions_undist2)))
-
-                for i1, line in enumerate(lines2):
-
-                    distances = [distance_point_line(x, line) for x in positions_undist2]
-                    cost[i1,:] = distances
-                    idx_sorted = np.argsort(distances)
-                    idxs_candidates = []
-                    sel_distances = []
-                    for idx in idx_sorted:
-                        # exit this loop if the distance start to be
-                        # o high or the number candidates is reached
-                        if verbose == 2:
-                            print("{}-{} {}-{} {:0.2f}".format(view1, i1, view2, idx, distances[idx]))
-                        if distances[idx] > max_dist:
-                            if verbose == 2:
-                                print("{}-{} {}-{} discarded because of distance {:0.2f}".format(view1, i1, view2, idx,
-                                                                                                 distances[idx]))
-                            else:
-                                break
-                        elif len(idxs_candidates) >= n_candidates:
-                            if verbose == 2:
-                                print("{}-{} {}-{} discarded because of number of candidates reached.".format(view1, i1,
-                                                                                                              view2,
-                                                                                                              idx))
-                            else:
-                                break
-                        else:
-                            if verbose == 2:
-                                print("{}-{} {}-{} selected distance {:0.2f}".format(view1, i1, view2, idx,
-                                                                                     distances[idx]))
-                            idxs_candidates.append(idx)
-                            sel_distances.append(distances[idx])
-
-                    sel_indexes[view1][view2].append((idxs_candidates, sel_distances))
+                cost, sel_ids = calc_cost_detections(positions_undist1, positions_undist2, F,
+                                                                        view1, view2, max_dist, n_candidates, verbose)
+                sel_indexes[view1][view2] = sel_ids
                 row_ind, col_ind = linear_sum_assignment(cost)
                 dist_array[view1][view2] = cost
+
+                # Calculate pose cost
+                poses_2 = poses[view2]
+                cost_poses = calc_cost_poses(poses_1, poses_2, F, kps_thres=0.8)
+
     return sel_indexes
+
 
 def filter_matching(views, matches):
     indexes = []
@@ -147,46 +194,123 @@ def filter_matching(views, matches):
             indexes.append([i,id[0][0]])
         except:
             pass
-    return np.array(indexes)
+
+    indexes = np.array(indexes)
+
+    # Remove duplicate values
+    for i in range(indexes.shape[0]):
+        id1 = indexes[i, 1]
+        for j in range(indexes.shape[0]):
+            # Case we have duplicate values
+            if i!=j and indexes[j, 1] == id1:
+
+                if matches['cam01']['cam02'][indexes[i, 0]][1][0] < matches['cam01']['cam02'][indexes[j, 0]][1][0] and len(matches['cam01']['cam02'][indexes[j, 0]][0]) > 1:
+                    matches['cam01']['cam02'][indexes[j, 0]][1].pop(0)
+                    matches['cam01']['cam02'][indexes[j, 0]][0].pop(0)
+                    indexes[j, 1] = matches['cam01']['cam02'][indexes[j, 0]][0][0]
+                elif matches['cam01']['cam02'][indexes[i, 0]][1][0] > matches['cam01']['cam02'][indexes[j, 0]][1][0] and len(matches['cam01']['cam02'][indexes[i, 0]][0]) > 1:
+                    matches['cam01']['cam02'][indexes[i, 0]][1].pop(0)
+                    matches['cam01']['cam02'][indexes[i, 0]][0].pop(0)
+                    indexes[i, 1] = matches['cam01']['cam02'][indexes[i, 0]][0][0]
 
 
+    return indexes
 
-def preview(img1,img2,d1,d2):
+
+def preview(img1, img2, d1, d2):
+
+    d1 = d1.astype(int)
+    d2 = d2.astype(int)
     for i in range(len(d1)):
         img1 = cv2.rectangle(img1, (d1[i, 0], d1[i, 1]), (d1[i, 2], d1[i, 3]), (0, 255, 0), 3)
         img1 = cv2.putText(img=img1,text=str(i),org=(d1[i, 0], d1[i, 1]),fontFace=cv2.FONT_HERSHEY_DUPLEX,fontScale=3.0,color=(125, 246, 55),thickness=3)
     for i in range(len(d2)):
         img2=cv2.rectangle(img2, (d2[i, 0], d2[i, 1]), (d2[i, 2], d2[i, 3]), (0, 255, 0), 3)
         img2=cv2.putText(img=img2,text=str(i),org=(d2[i, 0], d2[i, 1]),fontFace=cv2.FONT_HERSHEY_DUPLEX,fontScale=3.0,color=(125, 246, 55),thickness=3)
-    import matplotlib.pyplot as plt
+
     plt.imshow(np.hstack([img1,img2]))
     plt.show()
 
+
+def previewDetDict(img1, img2, dict1, dict2, kps_thres=0.7):
+    poses = dict1['poses']
+    dets = dict1['detections'].astype('int')
+    for i in range(len(dets)):
+        # Draw bboxes
+        cv2.rectangle(img1, (dets[i, 0], dets[i, 1]), (dets[i, 2], dets[i, 3]), (0, 255, 0), 3)
+        cv2.putText(img=img1,text=str(i),org=(dets[i, 0], dets[i, 1]),fontFace=cv2.FONT_HERSHEY_DUPLEX,fontScale=3.0,
+                    color=(125, 246, 55),thickness=3)
+
+        # Draw poses
+        for j in range(poses[i].shape[0]):
+            x, y = int(poses[i][j, 0] + dets[i][0]), int(poses[i][j, 1] + dets[i][1])
+            if poses[i][j, 2] > kps_thres:
+                cv2.circle(img1, (x, y), 4, (255, 0, 0), cv2.FILLED)
+            else:
+                cv2.circle(img1, (x, y), 4, (0, 255, 0), cv2.FILLED)
+
+    poses = dict2['poses']
+    dets = dict2['detections'].astype('int')
+    for i in range(len(dets)):
+        # Draw bboxes
+        cv2.rectangle(img2, (dets[i, 0], dets[i, 1]), (dets[i, 2], dets[i, 3]), (0, 255, 0), 3)
+        cv2.putText(img=img2, text=str(i), org=(int(dets[i, 0]), int(dets[i, 1])), fontFace=cv2.FONT_HERSHEY_DUPLEX,
+                           fontScale=3.0, color=(125, 246, 55), thickness=3)
+
+        # Draw poses
+        for j in range(poses[i].shape[0]):
+            x, y = int(poses[i][j, 0] + dets[i][0]), int(poses[i][j, 1] + dets[i][1])
+            if poses[i][j, 2] > kps_thres:
+                cv2.circle(img2, (x, y), 4, (255, 0, 0), cv2.FILLED)
+            else:
+                cv2.circle(img2, (x, y), 4, (0, 255, 0), cv2.FILLED)
+
+    plt.imshow(np.hstack([img1,img2]))
+    plt.show()
+
+
 if __name__ == '__main__':
-    d1= np.load('detections/detections_cam01.npy')
-    d2 = np.load('detections/detections_cam02.npy')
-    d3 = np.load('detections/detections_cam03.npy')
+    # d1= np.load('detections/detections_cam01.npy')
+    # d2 = np.load('detections/detections_cam02.npy')
+    # d3 = np.load('detections/detections_cam03.npy')
     img1 = np.load('detections/cam01_1stframe.npy')
     img2 = np.load('detections/cam02_1stframe.npy')
-    preview(img1,img2,d1,d2)
+    #preview(img1, img2, d1, d2)
+    with open("detections/detections_dict_cam01.pkl", "rb") as f:
+        dict1 = pickle.load(f)
+    with open("detections/detections_dict_cam02.pkl", "rb") as f:
+        dict2 = pickle.load(f)
+    d1 = dict1['detections']
+    d2 = dict2['detections']
+    previewDetDict(img1.copy(), img2.copy(), dict1, dict2)
 
     views = ['cam01', 'cam02']
-    D1,D2, D3 = [], [], []
+    calibration = load_calibration('calibration/')
+
+    # Use the centre of each bbox as detection.
+    D1, D2, D3 = [], [], []
     for i in range(len(d1)):
         D1.append(Detection2D(position=np.array([np.mean([d1[i,0],d1[i,2]]),np.mean([d1[i,1],d1[i,3]])]), confidence=d1[i,4],view=views[0]))
     for i in range(len(d2)):
         D2.append(Detection2D(position=np.array([np.mean([d2[i,0],d2[i,2]]),np.mean([d2[i,1],d2[i,3]])]), confidence=d2[i,4],view=views[1]))
     # for i in range(len(d3)):
     #     D3.append(Detection2D(position=np.array([np.mean([d3[i,0],d3[i,2]]),np.mean([d3[i,1],d3[i,3]])]), confidence=d3[i,4],view=views[2]))
-    calibration = load_calibration('calibration/')
     detections = {'cam01': D1, 'cam02':D2}
 
-    matches = find_candidate_matches(detections, views, calibration, max_dist=10, n_candidates=2,
-                           verbose=0)
+    # Add bbox offsets to poses
+    p1 = dict1['poses']
+    p2 = dict2['poses']
+    for i in range(d1.shape[0]):
+        p1[i][:, :2] = p1[i][:, :2] + d1[i][:2]
+    for i in range(d2.shape[0]):
+        p2[i][:, :2] = p2[i][:, :2] + d2[i][:2]
+    poses = {'cam01': p1, 'cam02': p2}
+
+    matches = find_candidate_matches(detections, poses, views, calibration, max_dist=10, n_candidates=2, verbose=0)
     sorted_idx = filter_matching(views, matches)
 
-    new_d1 = d1[sorted_idx[:,0],:]
-    new_d2 = d2[sorted_idx[:,1],:]
+    new_d1 = d1[sorted_idx[:, 0], :]
+    new_d2 = d2[sorted_idx[:, 1], :]
     preview(img1, img2, new_d1, new_d2)
 
     print(1)
